77603219a9095feae35b706787906fec6014c6b7
Edleron <Edleron@hotmail.com>
2023-02-18 02:57:33 +0300

    fix : Objects Movements

--- Assets/Scripts/ObjectsMovement.cs
+++ Assets/Scripts/ObjectsMovement.cs
@@ -4,34 +4,35 @@ using UnityEngine;
 
 public class ObjectsMovement : MonoBehaviour
 {
-    // Todo : proje başladıktan sonra, git uset değiştirilecek.
-    // Todo : Commitler bu sebeple gözükmüyor.
-    public float speed;
-    private BoxCollider2D box;
+    private float speed = 4.0f;
     private float groundWith;
     private float obstacleWidth;
 
+
     private void Start()
     {
+        // Bu script'i kullanan gameObject'in CompareTag keyword'u ile Tag'ni sorgulayarak 
+        // Ground'u mu yoksa, Obstaclemı sorusuna cevap arıyoruz.
         if (gameObject.CompareTag("Ground"))
         {
-            box = GetComponent<BoxCollider2D>();
-            groundWith = box.size.x;
+            groundWith = this.gameObject.GetComponent<BoxCollider2D>().size.x;
         }
-        else if (gameObject.CompareTag("Obstacle"))
+
+        if (gameObject.CompareTag("Obstacle"))
         {
-            obstacleWidth = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x;
+            obstacleWidth = this.gameObject.transform.GetChild(0).GetComponent<BoxCollider2D>().size.x;
         }
     }
 
     private void Update()
     {
-        if (GameManager.gameOver == false)
+        if (GameManager.isGameOver == false)
         {
             // Time.deltaTime, -> Bilgisayar sistemin'deki (ĞGpu, Cpu) zamanları eşitlemek için kullınlmaktadır.
             transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
         }
 
+        // Compare Tag, Bu script'i kullanan yapıların tag'i sorgulamaktadır.
         if (gameObject.CompareTag("Ground"))
         {
             if (transform.position.x <= -groundWith)
@@ -39,9 +40,11 @@ public class ObjectsMovement : MonoBehaviour
                 transform.position = new Vector2(transform.position.x + 2 * groundWith, transform.position.y);
             }
         }
-        else if (gameObject.CompareTag("Obstacle"))
+
+        // Compare Tag, Bu script'i kullanan yapıların tag'i sorgulamaktadır.
+        if (gameObject.CompareTag("Obstacle"))
         {
-            if (transform.position.x < GameManager.bottomLeft.x - obstacleWidth)
+            if (transform.position.x < GameManager.camArea.x - obstacleWidth)
             {
                 Destroy(gameObject);
             }
